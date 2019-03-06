63ecdaf0ad7faabdba4156edf395fcc2d6eefaec
Guadalupe Vazquez <guvadlt@gmail.com>
2019-03-06 10:15:28 -0600

    Se crearon todos los componentes de la ventana principal

--- src/Prueba.java
+++ src/Prueba.java
@@ -2,16 +2,31 @@ import java.awt.*;
 import javax.swing.*;
 
 class VentanaPrincipal extends JFrame {
-	
+	JTextArea txtAreaSi,txtAreaNo;
+	JButton btnIniciar;
 	public VentanaPrincipal(){
-		getContentPane().setLayout(new FlowLayout());
-		setSize(450, 500);
+		getContentPane().setLayout(null);
+		setSize(490, 450);
 		setTitle("Concurencia");
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE); 
 		setVisible(true);
 		
-		
+		JLabel lblSi=new JLabel("Respuestas en SI");
+		lblSi.setBounds(70,10,100,30);
+		add(lblSi);
+		JLabel lblNo =new JLabel("Respuestas en No");
+		lblNo.setBounds(290, 10, 110, 30);
+		add(lblNo);
+		txtAreaSi=new JTextArea();
+		txtAreaSi.setBounds(20, 50, 200, 200);
+		add(txtAreaSi);
+		txtAreaNo=new JTextArea();
+		txtAreaNo.setBounds(250, 50, 200, 200);
+		add(txtAreaNo);
+		btnIniciar=new JButton("Iniciar");
+		btnIniciar.setBounds(160, 280, 130, 30);
+		add(btnIniciar);
 	}
 }
 
