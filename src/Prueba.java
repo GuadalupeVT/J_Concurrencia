98142e232189536c85807434c9b76d4f75c8ca7a
Guadalupe Vazquez <guvadlt@gmail.com>
2019-03-06 17:47:01 -0600

    Se agrego un mejor metodo para seleccionar Si o No al azar y se optimizo para que terminara primero un hilo

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.*;
 
@@ -39,8 +40,13 @@ class HiloGenerarHistograma implements Runnable{
 				contadorNo++;
 			}
 		}
+		ventana.progressBarSi.setString(null);
+		ventana.progressBarSi.setStringPainted(true);
 		ventana.progressBarSi.setValue(contadorSi);
+		ventana.progressBarNo.setString(null);
+		ventana.progressBarNo.setStringPainted(true);
 		ventana.progressBarNo.setValue(contadorNo);
+		
 	}
 	
 }
@@ -67,7 +73,7 @@ class VentanaPrincipal extends JFrame{
 		txtAreaSi=new JTextArea();
 		txtAreaSi.setLineWrap(true);
 		txtAreaSi.setWrapStyleWord(true);
-		 JScrollPane scroll1=new JScrollPane(txtAreaSi);
+		JScrollPane scroll1=new JScrollPane(txtAreaSi);
 		scroll1.setBounds(20, 50, 200, 200);
 		add(scroll1);
 		txtAreaNo=new JTextArea();
@@ -84,28 +90,27 @@ class VentanaPrincipal extends JFrame{
 		JLabel lblRSi=new JLabel("Si");
 		lblRSi.setBounds(20,290,20,30);
 		add(lblRSi);
-		progressBarSi = new JProgressBar(0, 10000);
-		progressBarSi.setStringPainted(true);
+		progressBarSi = new JProgressBar(0, 100);
 		progressBarSi.setBounds(50, 290, 400, 30);
+		progressBarSi.setStringPainted(true);
+		progressBarSi.setString("Cargando......");
 		add(progressBarSi);
 		
 		JLabel lblRNo=new JLabel("No");
 		lblRNo.setBounds(20,340,20,30);
 		add(lblRNo);
-		progressBarNo = new JProgressBar(0, 10000);
-		progressBarNo.setStringPainted(true);
+		progressBarNo = new JProgressBar(0, 100);
 		progressBarNo.setBounds(50, 340, 400, 30);
+		progressBarNo.setStringPainted(true);
+		progressBarNo.setString("Cargando......");
 		add(progressBarNo);
 	}
 	
 	public String [] generarResultados() {
-		String [] resultados=new String[10000];
+		String [] resultados=new String[100];
+		String respuestas[]= {"No","Si","No","Si","Si","No"};
 		for (int i = 0; i < resultados.length; i++) {
-			if(Math.random()<0.5) {
-				resultados[i]="Si";
-			}else {
-				resultados[i]="No";
-			}
+		resultados[i] = respuestas[new Random().nextInt(respuestas.length)];
 		}
 		return resultados;
 	}
@@ -114,8 +119,6 @@ class VentanaPrincipal extends JFrame{
 }
 
 public class Prueba {
-	
-
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
@@ -123,14 +126,13 @@ public class Prueba {
 				Thread hiloImpresion=new Thread(new HiloImpresion(ventana));
 				Thread hiloGenerarHistograma=new Thread(new HiloGenerarHistograma(ventana));
 				hiloImpresion.start();
-				hiloGenerarHistograma.start();
 				try {
-					hiloGenerarHistograma.join();
+					hiloImpresion.join();
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-
+				hiloGenerarHistograma.start();
 			}
 		});
 
